e7a1eeab8f3e2b15eec2b595255d68f2121d006a
mveith <miroveith@windowslive.com>
2016-01-10 18:33:29 +0100

    sparta-calendar - running on heroku 2

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :main sparta-calendar.core
+
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
