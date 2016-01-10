aa194dc5b344376f65c21e8a0b177ffb6601ca33
mveith <miroveith@windowslive.com>
2016-01-10 18:08:32 +0100

    sparta-calendar - running on heroku

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sparta-calendar "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://mv1893.herokuapp.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -12,11 +12,17 @@
                  [org.omcljs/om "0.8.8"]
                  [ring "1.3.2"]
                  [compojure "1.3.1"]
-                 [enlive "1.1.6"]]
+                 [enlive "1.1.6"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.0"]]
+
+  :min-lein-version "2.0.0"
 
   :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.2.9"]]
+            [lein-figwheel "0.2.9"]
+            [environ/environ.lein "0.3.1"]]
 
+  :hooks [environ.leiningen.hooks]
 
   :source-paths ["src/clj" "src/cljs"]
   :resource-paths ["resources"]
@@ -24,6 +30,10 @@
                                     "resources/public/js/main.js"]
 
   :figwheel {:ring-handler sparta-calendar.core/handler}
+
+  :uberjar-name "sparta-calendar.jar"
+
+  ;:profiles {:production {:env {:production true}}})
 
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/clj" "src/cljs"]
--- src/clj/sparta_calendar/core.clj
+++ src/clj/sparta_calendar/core.clj
@@ -2,8 +2,11 @@
   (:require [ring.util.response :refer [file-response]]
             [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer [defroutes GET PUT]]
+            [compojure.handler :refer [site]]
             [compojure.route :as route]
-            [sparta-calendar.parser :as parser]))
+            [sparta-calendar.parser :as parser]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
 (defn index []
   (file-response "public/html/index.html" {:root "resources"}))
@@ -15,6 +18,11 @@
 (defn get-matches []
   (generate-response (pr-str (parser/load-matches))))
 
+(defroutes app
+           (GET "/" [] (index))
+           (GET "/matches" [] (get-matches))
+           (route/files "/" {:root "resources/public"}))
+
 (defroutes routes
            (GET "/" [] (index))
            (GET "/matches" [] (get-matches))
@@ -22,3 +30,8 @@
 
 (def handler
   (-> routes))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
+
